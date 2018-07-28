f870a19d0f643018332acc51908fe44f622e6466
Keith <wygulmage@users.noreply.github.com>
2018-07-28 10:01:04 -0400

    added RULES for Semicategory and Category.

--- src/Math/Semicategory.hs
+++ src/Math/Semicategory.hs
@@ -21,8 +21,6 @@ module Math.Semicategory (
   ,
   Semicategory(..)
   ,
-  Groupoid(..)
-  ,
   EveryObject
   ,
   Iso(..)
@@ -32,22 +30,26 @@ module Math.Semicategory (
   Opposite
   ) where
 
-import Data.Kind (Constraint, Type)
--- import Category.Flip
+import Data.Kind (
+  -- Constraint
+  -- ,
+  Type
+  )
+import Math.Opposite
 
 type Arrow1 o = o -> o -> Type
 
-newtype Flip (c :: Arrow1 o) y x = Flip { unFlip :: c x y }
+-- newtype Flip (c :: Arrow1 o) y x = Flip { unFlip :: c x y }
 
-data Iso (c :: Arrow1 o) x y = Iso { un :: c y x, run :: c x y }
+-- data Iso (c :: Arrow1 o) x y = Iso { un :: c y x, run :: c x y }
 
-type family Opposite (c :: Arrow1 o) :: Arrow1 o where
-  Opposite (Iso c) = Iso c -- no need for another wrapper
-  Opposite (Flip c) = c
-  Opposite c = Flip c
+-- type family Opposite (c :: Arrow1 o) :: Arrow1 o where
+--   Opposite (Iso c) = Iso c -- no need for another wrapper
+--   Opposite (Flip c) = c
+--   Opposite c = Flip c
 
 class Semicategory (c :: Arrow1 k) where
-  type Object c :: k -> Constraint
+  -- type Object c :: k -> Constraint
   opposite :: c x y -> Opposite c y x
   default opposite :: (Opposite c ~ Flip c) => c x y -> Opposite c y x
   opposite = Flip
@@ -56,32 +58,30 @@ class Semicategory (c :: Arrow1 k) where
   (◃) :: c y z → c x y → c x z
   g ◃ f = f ▹ g
 
+-- Good work-around with no obviously better design for built-in method inlining rules (https://ghc.haskell.org/trac/ghc/ticket/10595):
+__RULE__compose__ :: Semicategory c ⇒ c y z → c x y → c x z
+__RULE__compose__ = (◃)
+{-# INLINE [1] __RULE__compose__ #-}
+{-# RULES
+  "SPJ/compose" ∀ a b. a ◃ b = __RULE__compose__ a b;
+  "SPJ/flipped compose" ∀ a b. a ▹ b = __RULE__compose__ b a;
+  #-}
+{-# ANN __RULE__compose__ "HLint: ignore" #-}
+
 --- Opposite Semicategory ---
 instance (Semicategory c, Opposite c ~ Flip c) ⇒ Semicategory (Flip c) where
-  type Object (Flip c) = Object c
+  -- type Object (Flip c) = Object c
   opposite :: Flip c x y → c y x
   opposite = unFlip
   Flip f ◃ Flip g = Flip (f ▹ g)
 
 --- Isomorphisms in a semicategory ---
 instance Semicategory c ⇒ Semicategory (Iso c) where
-  type Object (Iso c) = Object c
+  -- type Object (Iso c) = Object c
   opposite (Iso u r) = Iso r u
   Iso u1 r1 ▹ Iso u2 r2 = Iso (u1 ◃ u2) (r1 ▹ r2)
 
 
-
---- Groupoid ---
-class Semicategory c ⇒ Groupoid c where
-  invert :: c x y → c y x
-
---- Opposite Groupoid ---
-instance (Groupoid c, Opposite c ~ Flip c) ⇒ Groupoid (Flip c) where
-  invert (Flip a) = Flip (invert a)
-
---- Isomorphisms in a groupoid ---
-instance Semicategory c ⇒ Groupoid (Iso c) where
-  invert = opposite
 
 --- Unconstrained object ---
 class EveryObject o
@@ -91,12 +91,10 @@ instance EveryObject o
 --- Examples ---
 
 instance Semicategory (,) where
-  type Object (,) = EveryObject
+  -- type Object (,) = EveryObject
   (l, _) ▹ (_, r) = (l, r)
 
-instance Groupoid (,) where
-  invert (x, y) = (y, x)
 
 instance Semicategory (→) where
-  type Object (→) = EveryObject
+  -- type Object (→) = EveryObject
   (f ◃ g) x = f (g x)
