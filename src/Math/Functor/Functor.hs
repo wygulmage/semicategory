8209a78f1ce988136834c46232b2287a101176e2
Keith <wygulmage@users.noreply.github.com>
2018-08-10 12:20:40 -0400

    switched to defining composition for included cats.
    type check of methods still does not terminate in GHCi, but it compiles fine.

--- src/Math/Functor/Functor.hs
+++ src/Math/Functor/Functor.hs
@@ -51,14 +51,67 @@ data NT :: ∀ i j. Arrow1 i → Arrow1 j → Arrow1 (i → j) where
 class
   (Category d, Category c) ⇒
   Functor (d :: Arrow1 i) (c :: Arrow1 j) (f :: i → j)
-  -- Functor d c f
   where
   fmap :: d x y → c (f x) (f y)
 
 class
   Functor (Opposite c) (NT c (→)) c ⇒
   Category (c :: Arrow1 i)
-  -- Category c
   where
   source :: c x y → c x x
   target :: c x y → c y y
+  (◃) :: c y z → c x y → c x z
+
+  -- (mostly) internal and defaults:
+  a ◃ b = runNT ((fmap :: Opposite c x y → NT c (→) (c x) (c y))
+                   (opposite b)) a
+  opposite :: c x y → Opposite c y x
+  default opposite :: Opposite c ~ Flip c ⇒ c x y → Opposite c y x
+  opposite = Flip
+
+
+
+----- Instances -----
+
+
+-- natural transformations:
+
+instance Functor (Flip (NT d c)) (NT (NT d c) (→)) (NT d c) where
+  fmap (Flip (NT a)) = NT (\(NT b) → NT (b ◃ a))
+
+instance Functor (NT d c) (→) (NT d c f) where
+  fmap = (◃)
+
+instance Category (NT d c) where
+  source = source
+  target = target
+  NT b ◃ NT a = NT (b ◃ a)
+
+
+-- flipped arrows:
+
+instance (Category c, Opposite c ~ Flip c) ⇒ Category (Flip c) where
+  source (Flip a) = Flip (target a)
+  target (Flip a) = Flip (source a)
+  opposite = unFlip
+  Flip b ◃ Flip a = Flip (a ◃ b)
+
+instance (Category c, Category (Flip c)) ⇒ Functor c (NT (Flip c) (→)) (Flip c) where
+  fmap a = NT (◃ Flip a)
+
+instance (Category c, Category (Flip c)) ⇒ Functor (Flip c) (→) (Flip c y) where
+  fmap = (◃)
+
+
+-- functions:
+
+instance Category (→) where
+  source _ x = x
+  target _ y = y
+  (g ◃ f) x = g (f x)
+
+instance Functor (Flip (→)) (NT (→) (→)) (→) where
+  fmap (Flip a) = NT (◃ a)
+
+instance Functor (→) (→) ((→) x) where
+  fmap = (◃)
