f411a49b22598e12eee430ce52e4599cb77dc93d
Keith <wygulmage@users.noreply.github.com>
2018-08-10 10:03:47 -0400

    chose a working NT definition.

--- src/Math/Functor/Functor.hs
+++ src/Math/Functor/Functor.hs
@@ -42,49 +42,11 @@ type family Opposite (f :: i → j → Type) :: j → i → Type where
   Opposite (Flip f) = f
   Opposite f = Flip f
 
--- data NT :: Arrow1 i → Arrow1 j → Arrow1 (i → j) where
---   NT ::
---     (Functor d c f, Functor d c g) ⇒
---     ∀ x. c (f x) (g x)
---     → NT d c f g
-
--- data NT (d :: Arrow1 i) (c :: Arrow1 j) (f :: i → j) (g :: i → j) where
---   NT ::
---     (Functor d c f, Functor d c g) ⇒
---     ∀ (x :: i). c (f x) (g x)
---     → NT d c f g
-
--- data NT (d :: Arrow1 i) (c :: Arrow1 j) (f :: i → j) (g :: i → j) where
---   NT :: ∀ d c f g.
---     (Functor d c f, Functor d c g) ⇒
---     ∀ (x :: i). (c :: Arrow1 j) ((f :: i → j) x) ((g :: i → j) x)
---     → NT (d :: Arrow1 i) (c :: Arrow1 j) (f :: i → j) (g :: i → j)
-
--- data NT d c f g where
---   NT ::
---     ∀ (d :: Arrow1 i) (c :: Arrow1 j) (f :: i → j) (g :: i → j).
---     (Functor d c f, Functor d c g) ⇒
---     ∀ (x :: i). c (f x) (g x)
---     → NT d c f g
-
--- data NT :: Arrow1 i → Arrow1 j → Arrow1 (i → j) where
---   NT ::
---     ∀ (d :: Arrow1 i) (c :: Arrow1 j) (f :: i → j) (g :: i → j).
---     (Functor d c f, Functor d c g) ⇒
---     ∀ x. c (f x) (g x)
---     → NT d c f g
-
 data NT :: ∀ i j. Arrow1 i → Arrow1 j → Arrow1 (i → j) where
   NT ::
     (Functor d c f, Functor d c g) ⇒
     {runNT :: ∀ (x :: i). c (f x) (g x)}
     → NT d c f g
-
--- data NT (d :: Arrow1 i) (c :: Arrow1 j) (f :: i → j) (g :: i → j) where
---   NT ::
---     (Functor d c f, Functor d c g) ⇒
---     {runNT :: ∀ x. c (f x) (g x)}
---     → NT d c f g
 
 class
   (Category d, Category c) ⇒
