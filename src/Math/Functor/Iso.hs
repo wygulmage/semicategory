cf2d74c0c8b84d585f9da02bc3d9d160b437977e
Keith <wygulmage@users.noreply.github.com>
2018-08-11 20:11:16 -0400

    added isomorphisms for undecidable cats.

---
+++ src/Math/Functor/Iso.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE
+  UnicodeSyntax
+  ,
+  NoImplicitPrelude
+  ,
+  TypeInType
+  -- PolyKinds
+  ,
+  ScopedTypeVariables
+  ,
+  TypeFamilies
+  ,
+  RankNTypes
+  ,
+  ConstraintKinds
+  ,
+  TypeOperators
+  ,
+  MultiParamTypeClasses
+  ,
+  FlexibleContexts
+  ,
+  FlexibleInstances
+  ,
+  DefaultSignatures
+  ,
+  GADTs
+  ,
+  UndecidableSuperClasses
+  ,
+  FunctionalDependencies
+  #-}
+
+module Math.Functor.Iso where
+
+import Math.Functor.Functor
+import Math.Functor.Terminal
+
+data Iso :: ∀ i. Arrow1 i → Arrow1 i where
+  Iso :: Category c ⇒ { un :: c y x, run :: c x y} → Iso c x y
+
+instance Category (Iso c) where
+  source (Iso u r) = Iso (target u) (source r)
+  target (Iso u r) = Iso (source u) (target r)
+  Iso u2 r2 ◃ Iso u1 r1 = Iso (u1 ◃ u2) (r2 ◃ r1)
+
+instance Functor (Flip (Iso c)) (NT (Iso c) (→)) (Iso c) where
+  fmap (Flip a) = NT (◃ a)
+
+instance Functor (Iso c) (→) (Iso c k) where
+  fmap = (◃)
+
+instance (Terminal c, Coterminal c, Ob1 c ~ Ob0 c) ⇒ Terminal (Iso c) where
+  type Ob1 (Iso c) = Ob1 c
+  arrow1 = Iso arrow0 arrow1
+
+instance (Terminal c, Coterminal c, Ob1 c ~ Ob0 c) ⇒ Coterminal (Iso c) where
+  type Ob0 (Iso c) = Ob0 c
+  arrow0 = Iso arrow1 arrow0
