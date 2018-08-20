e82773a8a3bda074cc36b25b801e65ff9b86e08c
Keith <wygulmage@users.noreply.github.com>
2018-08-20 11:43:36 -0400

    added dummy modules Functor and NT.

---
+++ src/Math/Category/Functor.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Math.Category.Functor
+  (Functor(..), Bifunctor, Profunctor, bimap, dimap)
+  where
+
+import Math.Category (Functor(..), Bifunctor, Profunctor, bimap, dimap)
+
+-- dummy to avoid circular module dependencies
---
+++ src/Math/Category/NT.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Math.Category.NT
+  (NT(..))
+  where
+
+import Math.Category (NT(..))
+
+-- dummy to avoid circular module dependencies
